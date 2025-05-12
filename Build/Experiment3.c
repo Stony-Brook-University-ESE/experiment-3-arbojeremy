b5f9ac411f76b7138862debf3fe67116910810dd
jarbo <jeremy.arboleda@stonybrook.edu>
2025-05-12 12:59:49 -0400

    push experiment 3

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int n = 1000;
-    int matrix[n][n];
-
+    int i, j;
+    
+    int **matrix[n][n] = malloc(n * sizeof(int *));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        matrix[i] = malloc(n * sizeof(int));
+    }
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             matrix[i][j] = i * n + j + 1;
         }
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
+    for (i = n - 1; i >= 0; i--) {
+        for (j = n - 1; j >= 0; j--) {
             printf("%4d ", matrix[i][j]);
         }
         printf("\n");
     }
 
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
     return 0;
 }
